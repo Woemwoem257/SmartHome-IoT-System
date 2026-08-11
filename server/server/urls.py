dea000fd76617f0541e9139e51050e7cc6eae49d
Woemwoem257 <ducnam257@gmail.com>
2026-08-11 16:49:03 +0700

    nghich

--- server/server/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')), # Trỏ đường dẫn gốc vào app dashboard
-]
